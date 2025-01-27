9750cae8598b998f5a3873bdf7144e5d56337221
uw819kt <miya0arima@gmail.com>
2025-01-27 12:42:54 +0900

    correction

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,10 +7,29 @@ set :bundle_without, %w{test}.join(':')
 set :linked_files, fetch(:linked_files, []).push('config/master.key')
 set :linked_files, %w{config/secrets.yml .env}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
-set :keep_releases, 5
+set :keep_releases, 2
 set :rbenv_version, '3.3.0'
 set :log_level, :info
 
+# after 'deploy:published', 'deploy:seed'   # 9
+after 'deploy:finished', 'deploy:restart'
+
+namespace :deploy do
+  desc 'Run seed'
+    task :seed do
+      on roles(:db) do
+        with rails_env: fetch(:rails_env) do
+          within current_path do
+            execute :bundle, :exec, :rake, 'db:seed'
+          end
+        end
+      end
+    end
+    desc 'Restart application'
+    task :restart do
+      invoke 'unicorn:restart'
+  end
+end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
