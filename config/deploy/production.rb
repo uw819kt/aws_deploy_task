9d049ca83b1063ad7ac9b9e1dd242d057dae7b6e
uw819kt <miya0arima@gmail.com>
2025-01-28 20:16:22 +0900

    correction

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,7 +49,7 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-server "13.208.246.233",
+server "15.168.30.122",
   user: "ec2-user",
   roles: %w{web db app},
   ssh_options: {
