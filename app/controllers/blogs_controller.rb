f70195918d511366a263b9e050bd7043926930aa
uw819kt <miya0arima@gmail.com>
2025-01-29 15:19:27 +0900

    correction

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -22,7 +22,7 @@ class BlogsController < ApplicationController
   # POST /blogs or /blogs.json
   def create
     @blog = Blog.new(blog_params)
-
+    @blog.thumbnail.attach(params[:blog][:thumbnail]) if @blog.thumbnail.blank?
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: "Blog was successfully created." }
